d744830347dbce29593e55c4c10086a46f0cd3d3
lizuncong <2900170896@qq.com>
2020-10-02 21:13:59 +0800

    stash:

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import mp4Src from './test.mp4';
 
 export default class Home extends React.Component {
   render() {
     return (
       <div className="container">
-        <video
-          controls
-          height="300"
-          width="500"
-        >
-          <source
-            src={mp4Src}
-            type="video/mp4"
-          />
-          <track kind="subtitles" src="foo.en.vtt" srcLang="en" label="English" />
-          <track kind="subtitles" src="foo.sv.vtt" srcLang="sv" label="Svenska" />
-          Sorry, your browser does not support embedded videos.
-        </video>
+        <div>首页</div>
       </div>
     );
   }
