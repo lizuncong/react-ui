e1074c98b151f69bbd430a822db5d4c15a728051
lizuncong <2900170896@qq.com>
2020-10-26 19:45:16 +0800

    stash

--- src/pages/less-demo/index.jsx
+++ src/pages/less-demo/index.jsx
@@ -1,23 +1,26 @@
-import React, { memo } from 'react';
+import React, { memo, useRef } from 'react';
 import './index.less';
 
-const Index = memo(() => (
-  <div className="less-demo">
-    <div className="demo-title">学习less高级语法</div>
-    <div className="description">
-      placeholder
-    </div>
-    <div className="wrap">
-      <div id="header">
-        <div>变量用法</div>
+const Index = memo(() => {
+  return (
+    <div className="less-demo">
+      <div className="demo-title">学习less高级语法</div>
+      <div className="description">
+        placeholder
       </div>
-      <div>
-        <div className="circle">1</div>
-        <div className="circle">2</div>
-        <div className="circle">3</div>
+      <div className="wrap">
+        <div id="header">
+          <div>变量用法</div>
+          <button className="button">按钮</button>
+        </div>
+        <div>
+          <div className="circle">1</div>
+          <div className="circle">2</div>
+          <div className="circle">3</div>
+        </div>
       </div>
     </div>
-  </div>
-));
+  );
+});
 
 export default Index;
