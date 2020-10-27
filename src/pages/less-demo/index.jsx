f3ae9d275e9e2178cd02080c1837399e3069c4d1
lizuncong <2900170896@qq.com>
2020-10-27 18:47:17 +0800

    stash

--- src/pages/less-demo/index.jsx
+++ src/pages/less-demo/index.jsx
@@ -1,26 +1,50 @@
-import React, { memo, useRef } from 'react';
+import React, {
+  memo, useRef, useState, useEffect,
+} from 'react';
 import './index.less';
 
-const Index = memo(() => {
+const Button = memo(({ }) => {
+  const [id, setId] = useState(0);
+  const myRef = useRef(null);
+  useEffect(() => {
+    myRef.current = setInterval(() => {
+      console.log('haha', id);
+    }, 1000);
+    return () => {
+      console.log('clear...');
+      clearInterval(myRef.current);
+    };
+  }, [id]);
   return (
-    <div className="less-demo">
-      <div className="demo-title">学习less高级语法</div>
-      <div className="description">
-        placeholder
-      </div>
-      <div className="wrap">
-        <div id="header">
-          <div>变量用法</div>
-          <button className="button">按钮</button>
-        </div>
-        <div>
-          <div className="circle">1</div>
-          <div className="circle">2</div>
-          <div className="circle">3</div>
-        </div>
-      </div>
+    <div
+      onClick={() => {
+        setId(id + 1);
+      }}
+    >
+      button{id}
     </div>
   );
 });
+
+const Index = memo(() => (
+  <div className="less-demo">
+    <div className="demo-title">学习less高级语法</div>
+    <div className="description">
+      placeholder
+    </div>
+    <div className="wrap">
+      <div id="header">
+        <div>变量用法</div>
+        <button className="button">按钮</button>
+      </div>
+      <div>
+        <div className="circle">1</div>
+        <div className="circle">2</div>
+        <div className="circle">3</div>
+      </div>
+    </div>
+    <Button />
+  </div>
+));
 
 export default Index;
