fd7e94aa1b1a72ba3f51e998140712eed6012c5d
lizuncong <2900170896@qq.com>
2020-10-28 19:49:43 +0800

    color

--- src/pages/less-demo/index.jsx
+++ src/pages/less-demo/index.jsx
@@ -1,50 +1,21 @@
-import React, {
-  memo, useRef, useState, useEffect,
-} from 'react';
+import React, { PureComponent } from 'react';
 import './index.less';
 
-const Button = memo(({ }) => {
-  const [id, setId] = useState(0);
-  const myRef = useRef(null);
-  useEffect(() => {
-    myRef.current = setInterval(() => {
-      console.log('haha', id);
-    }, 1000);
-    return () => {
-      console.log('clear...');
-      clearInterval(myRef.current);
-    };
-  }, [id]);
-  return (
-    <div
-      onClick={() => {
-        setId(id + 1);
-      }}
-    >
-      button{id}
-    </div>
-  );
-});
+class Uploader extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-const Index = memo(() => (
-  <div className="less-demo">
-    <div className="demo-title">学习less高级语法</div>
-    <div className="description">
-      placeholder
-    </div>
-    <div className="wrap">
-      <div id="header">
-        <div>变量用法</div>
-        <button className="button">按钮</button>
+  render() {
+    return (
+      <div
+        className="grid"
+      >
+        <div>1</div>
+        <div id="header">2</div>
       </div>
-      <div>
-        <div className="circle">1</div>
-        <div className="circle">2</div>
-        <div className="circle">3</div>
-      </div>
-    </div>
-    <Button />
-  </div>
-));
+    );
+  }
+}
 
-export default Index;
+export default Uploader;
