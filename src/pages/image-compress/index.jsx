a9d3e7fd1ebeec16ac214544c4564a2507320675
lizuncong <2900170896@qq.com>
2020-11-01 23:31:37 +0800

    粘贴事件

--- src/pages/image-compress/index.jsx
+++ src/pages/image-compress/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ImageCompress from 'components/img-upload';
+import Paste from './paste';
 import styles from './index.module.less';
 
 const Index = () => {
@@ -17,6 +18,28 @@ const Index = () => {
           }}
         />
       </div>
+      <Paste
+        disabled={false}
+        onPaste={(file) => {
+          console.log('file...', file);
+        }}
+      >
+        <div
+          className={styles.row}
+          onFocus={(e) => {
+            console.log('外部focus...', e.target);
+          }}
+        >
+          <div className={styles.title}>粘贴</div>
+          <ImageCompress
+            maxLength={5}
+            maxSize={300}
+            onChange={(files) => {
+              setFileObjs(files);
+            }}
+          />
+        </div>
+      </Paste>
     </div>
   );
 };
